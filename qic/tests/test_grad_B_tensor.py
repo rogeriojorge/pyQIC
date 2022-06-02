1ea2d69734c12ad78b030156b4034d6131589944
benagost <106112120+benagost@users.noreply.github.com>
2022-06-02 12:31:12 +0100

    QIC tests copied to the folder "tests"

---
+++ qic/tests/test_grad_B_tensor.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+
+import unittest
+import numpy as np
+import logging
+from qsc.qsc import Qsc
+
+logger = logging.getLogger(__name__)
+
+class GradBTensorTests(unittest.TestCase):
+
+    def a_test_axisymmetry(self):
+        """
+        Test that the grad B tensor is correct for an axisymmetric vacuum field
+        """
+        for sG in [-1, 1]:
+            for spsi in [-1, 1]:
+                for nfp in range(1, 4):
+                    for j in range(3):
+                        R0 = np.random.rand() * 2 + 0.3
+                        B0 = np.random.rand() * 2 + 0.3
+                        nphi = int(np.random.rand() * 10 + 3)
+                        etabar = (np.random.rand() - 0.5) * 4
+                        stel = Qsc(rc=[R0], zs=[0], nfp=nfp, nphi=nphi, sG=sG, \
+                                   spsi=spsi, B0=B0, etabar=etabar)
+                        val = sG * B0 / R0
+                        if np.mod(nphi, 2) == 0:
+                            nphi += 1
+
+                        rtol = 1e-13
+                        atol = 1e-13
+                        np.testing.assert_allclose(stel.grad_B_tensor.nt, np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(stel.grad_B_tensor.tn, np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(stel.grad_B_tensor.nn, np.zeros(nphi), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(stel.grad_B_tensor.bb, np.zeros(nphi), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(stel.grad_B_tensor.nb, np.zeros(nphi), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(stel.grad_B_tensor.bn, np.zeros(nphi), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(stel.L_grad_B, np.full(nphi, R0), rtol=rtol, atol=atol)
+                        
+class GradGradBTensorTests(unittest.TestCase):
+
+    def a_test_alternate_derivation_and_symmetry(self):
+        """
+        Compare Rogerio's derivation to mine to ensure the results
+        coincide.  Also verify symmetry in the first two components.
+        """
+        for sG in [-1, 1]:
+            for spsi in [-1, 1]:
+                for config in [1, 2, 3, 4, 5]:
+                    B0 = np.random.rand() * 0.4 + 0.8
+                    nphi = int(np.random.rand() * 50 + 61)
+                    s = Qsc.from_paper(config, sG=sG, spsi=spsi, B0=B0, nphi=65)
+                    s.calculate_grad_grad_B_tensor(two_ways=True)
+                    logger.info("Max difference between Matt and Rogerio's derivation for config {} is {}".format(config, np.max(np.abs(s.grad_grad_B - s.grad_grad_B_alt))))
+                    np.testing.assert_allclose(s.grad_grad_B, s.grad_grad_B_alt, rtol=1e-7, atol=1e-7)
+                    for i in range(3):
+                        for j in range(3):
+                            for k in range(3):
+                                # For all configs, the tensor should be symmetric in the 1st 2 indices:
+                                np.testing.assert_allclose(s.grad_grad_B[:, i, j, k], s.grad_grad_B[:, j, i, k], rtol=1e-8, atol=1e-8)
+                                # For curl-free fields, the tensor should also be symmetric in the last 2 indices:
+                                if config in {1, 2, 4}:
+                                    np.testing.assert_allclose(s.grad_grad_B[:, i, j, k], s.grad_grad_B[:, i, k, j], rtol=1e-8, atol=1e-8)
+            
+    def a_test_axisymmetry(self):
+        """
+        Test that the grad grad B tensor is correct for an axisymmetric vacuum field
+        """
+        B0_vals = [0.9, 1.1]
+        R0_vals = [0.85, 1.0, 1.2]
+        nphi_vals = [3, 5, 7, 9, 11]
+        etabar_vals = [0.55, 0.65, 0.95, 1.0, 1.09, 1.3, 1.4]
+        index = 0
+        for sG in [-1, 1]:
+            for spsi in [-1, 1]:
+                for nfp in range(1, 4):
+                    for j in range(3):
+                        """
+                        R0 = np.random.rand() * 2 + 0.3
+                        B0 = np.random.rand() * 2 + 0.3
+                        nphi = int(np.random.rand() * 10 + 3)
+                        etabar = (np.random.rand() - 0.5) * 4
+                        """
+                        # Try many combinations of values, in a deterministic way:
+                        R0 = R0_vals[np.mod(index, len(R0_vals))]
+                        B0 = B0_vals[np.mod(index, len(B0_vals))]
+                        nphi = nphi_vals[np.mod(index, len(nphi_vals))]
+                        etabar = etabar_vals[np.mod(index, len(etabar_vals))]
+                        index += 1
+                        
+                        s = Qsc(rc=[R0], zs=[0], nfp=nfp, nphi=nphi, sG=sG, \
+                                   spsi=spsi, B0=B0, etabar=etabar, I2=1.e-8, order='r2')
+                        # The O(r^2) solve fails if iota is exactly 0,
+                        # so above we set a tiny nonzero current to
+                        # create a tiny nonzero iota.
+                        if np.mod(nphi, 2) == 0:
+                            nphi += 1
+
+                        s.calculate_grad_grad_B_tensor(two_ways=True)
+                        # Check all components other than {tnn, ntn, nnt, ttt}. These should be 0.
+                        rtol = 1e-6
+                        atol = 1e-6
+                        for i in range(3):
+                            for j in range(3):
+                                for k in range(3):
+                                    if not ((i==2 and j==2 and k==2) or (i==2 and j==0 and k==0) or (i==0 and j==2 and k==0) or (i==i and j==0 and k==2)):
+                                        np.testing.assert_allclose(s.grad_grad_B[:,i,j,k], np.zeros(nphi), rtol=rtol, atol=atol)
+                                        np.testing.assert_allclose(s.grad_grad_B_alt[:,i,j,k], np.zeros(nphi), rtol=rtol, atol=atol)
+                                        
+                        # Check ttt component
+                        val = -2 * sG * B0 / (R0 * R0)
+                        np.testing.assert_allclose(s.grad_grad_B[:,2,2,2], np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(s.grad_grad_B_alt[:,2,2,2], np.full(nphi, val), rtol=rtol, atol=atol)
+
+                        # Check tnn, ntn, and nnt components:
+                        val = 2 * sG * B0 / (R0 * R0)
+                        np.testing.assert_allclose(s.grad_grad_B[:,2,0,0], np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(s.grad_grad_B[:,0,2,0], np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(s.grad_grad_B[:,0,0,2], np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(s.grad_grad_B_alt[:,2,0,0], np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(s.grad_grad_B_alt[:,0,2,0], np.full(nphi, val), rtol=rtol, atol=atol)
+                        np.testing.assert_allclose(s.grad_grad_B_alt[:,0,0,2], np.full(nphi, val), rtol=rtol, atol=atol)
+                        
+
+class CylindricalCartesianTensorsTests(unittest.TestCase):
+
+    def test_cylindrical_cartesian_tensors(self):
+        """
+        Test the accuracy and the symmetry in the the cylindrical and Cartesian
+        versions of the grad B and grad grad B tensors.
+        """
+        # Test the grad B tensor in axisymmetry, including symmetry in both indices
+        Rmajor = np.random.rand() * 2 + 0.3
+        B0 = np.random.rand() * 2 + 1.0
+        stel = Qsc(rc=[Rmajor, 0.], zs=[0, 0.], etabar=1.0, B0=B0)
+        factor = stel.B0 / Rmajor
+        dBdx_cylindrical = stel.grad_B_tensor_cylindrical
+        np.testing.assert_almost_equal(dBdx_cylindrical[0, 0], np.zeros(stel.nphi))
+        np.testing.assert_almost_equal(dBdx_cylindrical[0, 1], np.full(stel.nphi, -factor))
+        np.testing.assert_almost_equal(dBdx_cylindrical[1, 1], np.zeros(stel.nphi))
+        np.testing.assert_almost_equal(dBdx_cylindrical, dBdx_cylindrical.transpose(1, 0, 2))
+        dBdx_cartesian = stel.grad_B_tensor_cartesian()
+        np.testing.assert_almost_equal(dBdx_cartesian[0, 0], np.full(stel.nphi, factor * np.sin(2 * stel.phi)))
+        np.testing.assert_almost_equal(dBdx_cartesian[0, 1], np.full(stel.nphi, -factor * np.cos(2 * stel.phi)))
+        np.testing.assert_almost_equal(dBdx_cartesian[1, 1], np.full(stel.nphi, -factor * np.sin(2 * stel.phi)))
+        np.testing.assert_almost_equal(dBdx_cartesian, dBdx_cartesian.transpose(1, 0, 2))
+
+        # Test the grad B tensor for another configuration, including symmetry in both indices
+        stel = Qsc.from_paper(1)
+        dBdx_cylindrical = stel.grad_B_tensor_cylindrical
+        np.testing.assert_almost_equal(dBdx_cylindrical, dBdx_cylindrical.transpose(1, 0, 2))
+        dBdx_cartesian = stel.grad_B_tensor_cartesian()
+        np.testing.assert_almost_equal(dBdx_cartesian, dBdx_cartesian.transpose(1, 0, 2))
+
+        # Test the grad grad B tensor, including symmetry in three indices
+        dBdxdx_cylindrical = stel.grad_grad_B_tensor_cylindrical()
+        np.testing.assert_almost_equal(np.transpose(stel.grad_grad_B, (1, 2, 3, 0)), dBdxdx_cylindrical)
+        dBdxdx_cylindrical_transpose_1 = dBdxdx_cylindrical.transpose(0, 2, 1, 3)
+        dBdxdx_cylindrical_transpose_2 = dBdxdx_cylindrical.transpose(1, 0, 2, 3)
+        dBdxdx_cylindrical_transpose_3 = dBdxdx_cylindrical.transpose(1, 2, 0, 3)
+        dBdxdx_cylindrical_transpose_4 = dBdxdx_cylindrical.transpose(2, 0, 1, 3)
+        dBdxdx_cylindrical_transpose_5 = dBdxdx_cylindrical.transpose(2, 1, 0, 3)
+        np.testing.assert_almost_equal(dBdxdx_cylindrical_transpose_1, dBdxdx_cylindrical)
+        np.testing.assert_almost_equal(dBdxdx_cylindrical_transpose_2, dBdxdx_cylindrical)
+        np.testing.assert_almost_equal(dBdxdx_cylindrical_transpose_3, dBdxdx_cylindrical)
+        np.testing.assert_almost_equal(dBdxdx_cylindrical_transpose_4, dBdxdx_cylindrical)
+        np.testing.assert_almost_equal(dBdxdx_cylindrical_transpose_5, dBdxdx_cylindrical)
+
+        dBdxdx_cartesian = stel.grad_grad_B_tensor_cartesian()
+        dBdxdx_cartesian_transpose_1 = dBdxdx_cartesian.transpose(0, 2, 1, 3)
+        dBdxdx_cartesian_transpose_2 = dBdxdx_cartesian.transpose(1, 0, 2, 3)
+        dBdxdx_cartesian_transpose_3 = dBdxdx_cartesian.transpose(1, 2, 0, 3)
+        dBdxdx_cartesian_transpose_4 = dBdxdx_cartesian.transpose(2, 0, 1, 3)
+        dBdxdx_cartesian_transpose_5 = dBdxdx_cartesian.transpose(2, 1, 0, 3)
+        np.testing.assert_almost_equal(dBdxdx_cartesian_transpose_1, dBdxdx_cartesian)
+        np.testing.assert_almost_equal(dBdxdx_cartesian_transpose_2, dBdxdx_cartesian)
+        np.testing.assert_almost_equal(dBdxdx_cartesian_transpose_3, dBdxdx_cartesian)
+        np.testing.assert_almost_equal(dBdxdx_cartesian_transpose_4, dBdxdx_cartesian)
+        np.testing.assert_almost_equal(dBdxdx_cartesian_transpose_5, dBdxdx_cartesian)
+                
+if __name__ == "__main__":
+    unittest.main()
---
+++ qic/tests/test_omnigenity_r2.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+import unittest
+import numpy as np
+from qsc.qsc import Qsc
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class NewtonTests(unittest.TestCase):
+
+    def test_omnigenity(self):
+        # Order r2
+        nphi=351
+        # rc      = [ 1.0,0.0,-0.2,0.0,0.0,0.0,0.0,0.0,0.0 ]
+        # zs      = [ 0.0,0.0,0.3052828826418424,0.0,-0.0062592338816644544,0.0,-0.0013608816560281526,0.0,0.00034499224916859685 ]
+        # B0_vals = [ 1.0,0.20356847942029987 ]
+        # d_svals = [ 0.0,0.0,0.0,0.0 ]
+        # k_second_order_SS   = 10.767959843661382
+        # delta   = 1.756279248056422
+        # nfp     = 1
+        # B2s_svals = [ 0.0,-0.2890116275360059,0.42365891429851427,0.19804239404493473,-0.04647331768552959 ]
+        # B2c_cvals = [ -0.07443435202732489,0.059675356537100585,-0.42166691507263726,0.10299024291977077,-0.2222054479974802 ]
+        # p2      =  0.0
+        # stel    =  Qsc(rc=rc,zs=zs, nfp=nfp, B0_vals=B0_vals, d_svals=d_svals, nphi=nphi, omn=True, delta=delta, B2c_cvals=B2c_cvals, B2s_svals=B2s_svals, p2=p2, order='r2', k_second_order_SS=k_second_order_SS)
+        rc      = [ 1.0,0.0,-0.2,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0 ]
+        zs      = [ 0.0,0.0,0.3515256195485459,0.0,-0.025760473197283567,0.0,0.0012843427458089259,0.0,-0.0010382838326711225,0.0,5.8831297609230655e-05,0.0,-2.5206215753153992e-05,0.0,1.0528739193321092e-05,0.0,1.6498256568592327e-06,0.0,7.231278664950175e-07,0.0,6.236575496049474e-08 ]
+        B0_vals = [ 1.0,0.2898599196464543 ]
+        k_second_order_SS   = 3.8393349190026678
+        d_svals = [0.]
+        delta   = 1.657971751685515
+        nfp     = 1
+        B2s_svals = [ 0.0,0.02516989814033407,0.03283462033677047,0.004837653606915959,0.03466436789051,0.012588451775448475,0.0034798016087720404,0.012368193922088775,0.03844118710392006,0.002403469150739275,0.01163670504914525 ]
+        B2c_cvals = [ 0.0206121680245761,0.0602654567737221,0.06344485624821924,0.026187767224228442,0.05839086377958894,0.007915937165665798,0.036743548309213274,0.036842540401104525,0.0142687978041074,-0.013198287181027281,-0.0072079191108149875,0.0 ]
+        B2s_cvals = [ -0.1318900156971933,0.5649273497420052,-0.022473047295254987,-0.08265049514305536,0.41342725761575694,-0.2931657967710277,0.31542024309141736,-0.21005748857764805,0.6787636103131851,-0.1240125056565712,0.1242848766908088,0.0 ]
+        B2c_svals = [ 0.0,1.5753225284964127,-0.57516292896785,0.8948531647751348,-3.0875825530983967,-0.06329756906736621,-1.7890702692964318,1.039759175233141,-0.1690212827616995,-0.16762613422413772,0.015322897225364228,0.0,0.0 ]
+        p2      =  0.0
+        stel    =  Qsc(rc=rc,zs=zs, nfp=nfp, B0_vals=B0_vals, d_svals=d_svals, nphi=nphi, omn=True, delta=delta, B2c_cvals=B2c_cvals, B2s_svals=B2s_svals, p2=p2, order='r3', k_second_order_SS=k_second_order_SS, B2s_cvals=B2s_cvals, B2c_svals=B2c_svals)
+        import matplotlib.pyplot as plt
+        # plt.plot(stel.X1c, label='X1c')
+        # plt.plot(stel.X1s, label='X1s')
+        # plt.plot(stel.Y1c, label='Y1c')
+        # plt.plot(stel.Y1s, label='Y1s')
+        # plt.plot(stel.sigma, label='sigma')
+        # plt.plot(stel.d, label='d')
+        # plt.plot(stel.d_bar, label='d_bar')
+        # plt.plot(stel.alpha, label='alpha')
+        # plt.plot(stel.X20, label='X20')
+        # plt.plot(stel.X2c, label='X2c')
+        # plt.plot(stel.X2s, label='X2s')
+        # plt.plot(stel.Y20, label='Y20')
+        # plt.plot(stel.Y2c, label='Y2c')
+        # plt.plot(stel.Y2s, label='Y2s')
+        # plt.plot(stel.B20, label='B20')
+        # plt.plot(stel.B2c_array, label='B2c_array')
+        # plt.plot(stel.B2s_array, label='B2s_array')
+        plt.figure()
+        plt.plot(stel.B20, label='B20')
+        plt.plot(stel.B20[::-1], label='B20 reversed')
+        plt.plot(stel.B20QI_deviation, label='B20 deviation')
+        plt.legend()
+        plt.figure()
+        plt.plot(stel.B2cQI, label='B2cQI')
+        plt.plot(stel.B2cQI[::-1], label='B2cQI reversed')
+        plt.plot(stel.B2cQI_deviation, label='B2cQI deviation')
+        plt.legend()
+        plt.figure()
+        plt.plot(stel.B2sQI, label='B2sQI')
+        plt.plot(stel.B2sQI[::-1], label='B2sQI reversed')
+        plt.plot(stel.B2sQI_deviation, label='B2sQI deviation')
+        plt.legend()
+        plt.show()
+        # stel.plot()
+        # stel.plot_boundary(r=0.05,fieldlines=True)
+        
+if __name__ == "__main__":
+    unittest.main()
