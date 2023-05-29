grad_grad_B tests and fixed sign in sigma equation

qic/calculate_r1.py
@@ -131,7 +131,7 @@ def _residual(self, x):
         self.gamma = iota + self.helicity * self.nfp - np.matmul(self.d_d_varphi, self.alpha)
     r = np.matmul(self.d_d_varphi, sigma) + self.gamma * \
         (self.etabar_squared_over_curvature_squared * self.etabar_squared_over_curvature_squared + 1 + sigma * sigma) \
-        - 2 * self.etabar_squared_over_curvature_squared * (-self.spsi * self.torsion + self.I2 / self.Bbar) * self.G0 / self.B0
+        - 2 * self.etabar_squared_over_curvature_squared * (- self.torsion + self.I2 / self.Bbar) * self.G0 / self.B0
     #logger.debug("_residual called with x={}, r={}".format(x, r))
 
     sigma_at_0 = self.interpolateTo0 @ sigma
@@ -233,8 +233,8 @@ def r1_diagnostics(self):
         cosangle = np.cos(angle)
         B1sQI    = 0    # B=B0*(1+d cos(theta-alpha)), B1cQI = B0*d
         B1cQI    = self.B0 * self.d
-        self.B1c = -(B1cQI * cosangle - B1sQI * sinangle)
-        self.B1s = -(B1sQI * cosangle + B1cQI * sinangle)
+        self.B1c = (B1cQI * cosangle - B1sQI * sinangle)
+        self.B1s = (B1sQI * cosangle + B1cQI * sinangle)
     else:
         self.alpha_tilde = self.alpha
         self.cos_alpha_tilde_spline = self.convert_to_spline(np.cos(self.alpha))

qic/qic.py
@@ -373,7 +373,7 @@ class Qic():
             delta   = 0.1 * 2*np.pi
             nphi    = 151
             add_default_args(kwargs, rc=rc, zs=zs, nfp=1, B0_vals=B0_vals, d_svals=d_svals, nphi=nphi, omn=True, delta=delta)
-        elif name == "QI r1 Jorge" or name == "QI NFP1 r1 Jorge" or name == "QI Jorge":
+        elif name == "QI r1 Jorge" or name == "QI NFP1 r1 Jorge" or name == "QI Jorge" or name == 6:
             """ The configuration from Jorge et al (2022) """
             rc      = [ 1.0,0.0,-0.4056622889934463,0.0,0.07747378220100756,0.0,-0.007803860877024245,0.0,0.0,0.0,0.0,0.0,0.0 ]
             zs      = [ 0.0,0.0,-0.24769666390049602,0.0,0.06767352436978152,0.0,-0.006980621303449165,0.0,-0.0006816270917189934,0.0,-1.4512784317099981e-05,0.0,-2.839050532138523e-06 ]

qic/tests/test_grad_B_tensor.py
@@ -4,12 +4,13 @@ import unittest
 import numpy as np
 import logging
 from qic.qic import Qic
+from qsc import Qsc
 
 logger = logging.getLogger(__name__)
 
 class GradBTensorTests(unittest.TestCase):
 
-    def a_test_axisymmetry(self):
+    def test_axisymmetry(self):
         """
         Test that the grad B tensor is correct for an axisymmetric vacuum field
         """
@@ -39,28 +40,36 @@ class GradBTensorTests(unittest.TestCase):
                         
 class GradGradBTensorTests(unittest.TestCase):
 
-    def a_test_alternate_derivation_and_symmetry(self):
+    def test_alternate_derivation_and_symmetry(self):
         """
         Compare Rogerio's derivation to mine to ensure the results
         coincide.  Also verify symmetry in the first two components.
         """
+        rtol = 1e-4
+        atol = 1e-4
         for sG in [-1, 1]:
             for spsi in [-1, 1]:
-                for config in [1, 2, 3, 4, 5]:
+                for config in [1, 2, 3, 4, 5, 6]:
                     B0 = np.random.rand() * 0.4 + 0.8
-                    nphi = int(np.random.rand() * 50 + 61)
-                    s = Qic.from_paper(config, sG=sG, spsi=spsi, B0=B0, nphi=65)
-                    s.calculate_grad_grad_B_tensor(two_ways=True)
+                    nphi = 501 # int(np.random.rand() * 50 + 61)
+                    s = Qic.from_paper(config, sG=sG, spsi= spsi, B0=B0, nphi=nphi, order = 'r2')
+                    s.calculate_grad_grad_B_tensor(two_ways = True)
                     logger.info("Max difference between Matt and Rogerio's derivation for config {} is {}".format(config, np.max(np.abs(s.grad_grad_B - s.grad_grad_B_alt))))
-                    np.testing.assert_allclose(s.grad_grad_B, s.grad_grad_B_alt, rtol=1e-7, atol=1e-7)
+                    np.testing.assert_allclose(s.grad_grad_B, s.grad_grad_B_alt, rtol=rtol, atol=atol)
+                    if config in [1, 2, 3, 4, 5]:
+                        s_qs = Qsc.from_paper(config, sG=sG, spsi= spsi, B0=B0, nphi=nphi, order = 'r2')
+                        s_qs.calculate_grad_grad_B_tensor(two_ways = True)
+                        logger.info("Max difference between qsc and qic for config {} is {}".format(config, np.max(np.abs(s.grad_grad_B - s.grad_grad_B_alt))))
+                        np.testing.assert_allclose(s.grad_grad_B, s.grad_grad_B_alt, rtol=rtol, atol=atol)
+
                     for i in range(3):
                         for j in range(3):
                             for k in range(3):
                                 # For all configs, the tensor should be symmetric in the 1st 2 indices:
-                                np.testing.assert_allclose(s.grad_grad_B[:, i, j, k], s.grad_grad_B[:, j, i, k], rtol=1e-8, atol=1e-8)
+                                np.testing.assert_allclose(s.grad_grad_B[:, i, j, k], s.grad_grad_B[:, j, i, k], rtol=rtol, atol=atol)
                                 # For curl-free fields, the tensor should also be symmetric in the last 2 indices:
                                 if config in {1, 2, 4}:
-                                    np.testing.assert_allclose(s.grad_grad_B[:, i, j, k], s.grad_grad_B[:, i, k, j], rtol=1e-8, atol=1e-8)
+                                    np.testing.assert_allclose(s.grad_grad_B[:, i, j, k], s.grad_grad_B[:, i, k, j], rtol=rtol, atol=atol)
             
     def a_test_axisymmetry(self):
         """