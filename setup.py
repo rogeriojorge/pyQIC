45758256be1c134b31f57e2e8b65cd74951cefb2
Rogerio Jorge <rogerio.jorge@wisc.edu>
2026-01-30 15:52:04 -0600

    Bump version from 0.3.4 to 0.3.6 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="qicna",
-    version="0.3.4",
+    version="0.3.6",
     author="Rogerio Jorge and Matt Landreman",
     author_email="rogerio.jorge@tecnico.ulisboa.pt",
     description="Quasi-isodynamic Stellarator Construction",
